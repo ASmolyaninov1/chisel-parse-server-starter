bff8c1b98aa276cc61088f24432318e672e8df25
ASmolyaninov <a.smolyaninov@brightlab.me>
2022-01-26 17:00:37 +0300

    little fixes

--- cloud/main.js
+++ cloud/main.js
@@ -1,44 +1,41 @@
 console.log('Cloud code connected')
-Promise.all([
-  import('capture-website'),
-  import('colorthief'),
-  import('pdf2pic')
-]).then((res) => {
-  const [captureWebsite, ColorThief, pdf] = res
-  const getPaletteByImageAndRemove = async imagePath => {
-    console.log('imagePath => ', imagePath)
 
-    let result
-    try {
-      result = await ColorThief.getPalette(imagePath)
-    } catch (err) {
-      result = { status: 500, message: 'Get palette error' }
-    }
+const getPaletteByImageAndRemove = async imagePath => {
+  const ColorThief = await import('colorthief')
+  const fs = await import('fs')
 
-    fs.unlink(imagePath, err => console.log('File remove error => ', err));
-    return result
+  let result
+  try {
+    result = await ColorThief.getPalette(imagePath)
+  } catch (err) {
+    result = { status: 500, message: 'Get palette error' }
   }
 
-  Parse.Cloud.define('getPalette', async request => {
-    const params = request.params;
-    const brandUrl = params?.brandUrl;
+  fs.unlink(imagePath, err => console.log('File remove error => ', err));
+  return result
+}
 
-    if (!brandUrl) {
-      return { status: 400, message: 'Please provide field brandUrl' };
-    }
+Parse.Cloud.define('getPalette', async request => {
+  const params = request.params;
+  const brandUrl = params?.brandUrl;
+  const captureWebsite = await import('capture-website')
 
-    const filename = brandUrl.replace(/[^a-z0-9]/gi, '_').toLowerCase();
-    const filePath = `./${filename}.png`;
+  if (!brandUrl) {
+    return { status: 400, message: 'Please provide field brandUrl' };
+  }
 
-    try {
-      await captureWebsite.file(brandUrl, filePath, { fullPage: true });
-    } catch (err) {
-      console.log('Invalid brand url', err)
-      return { status: 400, message: 'Invalid brand url' };
-    }
+  const filename = brandUrl.replace(/[^a-z0-9]/gi, '_').toLowerCase();
+  const filePath = `./${filename}.png`;
 
-    return await getPaletteByImageAndRemove(filePath)
-  })
+  try {
+    await captureWebsite.file(brandUrl, filePath, { fullPage: true });
+  } catch (err) {
+    console.log('Invalid brand url', err)
+    return { status: 400, message: 'Invalid brand url' };
+  }
+
+  return await getPaletteByImageAndRemove(filePath)
+})
 
 // Parse.Cloud.define('getPdfPalette', request => {
 //   const params = request.params
@@ -46,4 +43,3 @@ Promise.all([
 //   fs.writeFile
 //   fromBase64(pdf, { savePath: './' }).convert()
 // })
-})
