131b1d6b325743728ca21151f745da96c9130c1d
ASmolyaninov <a.smolyaninov@brightlab.me>
2022-02-01 17:18:02 +0300

    little fixes

--- cloud/palette.js
+++ cloud/palette.js
@@ -19,7 +19,6 @@ Parse.Cloud.define('getPalette', async request => {
   const brandUrl = params?.brandUrl;
 
   const captureWebsite = await import("capture-website")
-  console.log('captureWebsite file function => ', captureWebsite.default)
 
   if (!brandUrl) {
     return { status: 400, message: 'Please provide field brandUrl' };
