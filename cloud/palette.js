587053de36f3fd6e3564121995105c63d7f5655b
ASmolyaninov <a.smolyaninov@brightlab.me>
2022-02-07 13:02:29 +0300

    little fixes

--- cloud/palette.js
+++ cloud/palette.js
@@ -35,7 +35,7 @@ Parse.Cloud.define('getPdfScreenshot', async request => {
   const pdf = params.pdf
 
   const pdf2pic = await import('pdf2pic')
-  const pdfImage = await pdf2pic.default.fromBase64(pdf).bulk(-1, true)
+  const pdfImage = await pdf2pic.default.fromBase64(pdf)(1, true)
   console.log('pdfImage => ', pdfImage)
 
   return { status: 200, pdfImage }
